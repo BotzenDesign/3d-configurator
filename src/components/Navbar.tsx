9d923a5d047fd40dc3e733c0c2a1b0a29c57a214
raza-ahmed-khan360 <razaahmedkhan360@gmail.com>
2026-05-23 00:44:14 +0500

    logo updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ export default function Navbar() {
   return (
     <nav className="h-12 bg-panel-bg border-b border-border flex items-center px-4 gap-6 z-20">
       <a href="/" className="flex items-center gap-2">
+      <img src="favicon.ico" className="h-10"/>
         <div className="text-lg font-bold tracking-tight text-foreground uppercase">
           Botzen<span className="text-muted-foreground font-medium">Design</span>
         </div>
